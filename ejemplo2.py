6eebd59aac27e39509098630051fbc4e895136e8
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2024-11-14 21:16:35 -0500

    Nuevos ejemplos

---
+++ ejemplo2.py
@@ -0,0 +1,20 @@
+import math
+#Crear un programa donde calculemos el área del circulo
+radio = float(input("Ingrese el radio del círculo:\n"))
+#El calculo del área del circulo (Ärea = pi ^ 2)
+
+area = math.pi * (radio ** 2)
+
+area = math.pi * pow(radio,2)
+
+
+
+print(f"El área del circulo es: {area:.2f}")
+
+print("El Ärea del circulo es:",round(area,2))
+
+      
+      
+      
+
+
---
+++ ejemplo3.py
@@ -0,0 +1,16 @@
+edades = input("Ingrese 4 edades:\n").split()
+print(edades)
+edad1 = int(edades[0])
+edad2 = int(edades[1])
+edad3 = int(edades[2])
+edad4 = int(edades[3])
+
+print(edad1)
+print(edad2)
+print(edad3)
+print(edad4)
+
+edad_maxima = max(edad1,edad2,edad3)
+print("La edad máxima es: ", edad_maxima)
+
+
