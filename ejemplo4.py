02fddcaafd1a8c28d110092a3adb6c773aab7fa9
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2024-11-14 21:31:40 -0500

    Nuevos ejemplos

---
+++ ejemplo4.py
@@ -0,0 +1,21 @@
+#Crear un programa donde vamos a calular el IMC
+
+print("Ingrese el nombre del paciente:")
+nombre = input()
+
+
+nombre = input("Ingres el nombre del paciente:\n")
+peso = float(input("Ingrese el peso del paciente:\n"))
+altura = float(input("Ingrese la altura del paciente:\n"))
+
+#Formula(IMC = Peso / (Altura ** 2))
+
+imc = peso / (altura ** 2)
+
+print(f"Bienvenido señor(@): {nombre.upper()}")
+print(f"Su indice de masa corporal es: {imc:.2f}")
+
+
+
+
+
