537684ae44b9499ee579e74ae81803d950f7e5ab
percyjavier1977 <javierizquierdopajuelo@gmail.com>
2024-11-14 20:34:16 -0500

    Primer ejemplo

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+#Crear un programa donde vamos a converit la temperatura de celsius a fahrenheit
+celsius = int(input("Ingrese una cantidad es celsius: "))
+
+#Calcular la temperatura en Fahrenheit (F = C * 9/5 + 32)
+
+fa = (celsius * 9/5) + 32
+
+#Imprimir el resultado
+print("La temperatura en Fahrenheit es:",fa)
+
+
+
